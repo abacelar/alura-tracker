cd35b8f099bfb638aee1d1b2baff5124af36111f
Andre Bacelar <abacelar@outlook.com>
2022-09-26 22:01:43 -0300

    Refactored Formulario

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -20,7 +20,7 @@
 <script lang="ts">
 import { TipoNotificacao } from "@/interfaces/INotificacao";
 import { useStore } from "@/store";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import useNotificador from "@/hooks/notificador";
 import { ALTERAR_PROJETO, CADASTRAR_PROJETO } from "@/store/tipo-acoes";
 
@@ -30,19 +30,6 @@ export default defineComponent({
     id: {
       type: String,
     },
-  },
-  mounted() {
-    if (this.id) {
-      const projeto = this.store.state.projeto.projetos.find(
-        (proj) => proj.id == this.id
-      );
-      this.nomeDoProjeto = projeto?.nome || "";
-    }
-  },
-  data() {
-    return {
-      nomeDoProjeto: "",
-    };
   },
   methods: {
     salvar() {
@@ -71,12 +58,23 @@ export default defineComponent({
       this.$router.push("/projetos");
     },
   },
-  setup() {
+  setup(props) {
     const store = useStore();
     const { notificar } = useNotificador();
+
+    const nomeDoProjeto = ref("");
+
+    if (props.id) {
+      const projeto = store.state.projeto.projetos.find(
+        (proj) => proj.id == props.id
+      );
+      nomeDoProjeto.value = projeto?.nome || "";
+    }
+
     return {
       store,
       notificar,
+      nomeDoProjeto,
     };
   },
 });
