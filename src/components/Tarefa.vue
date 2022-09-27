0c349fe2aa3f7b798c66f3f75165740f3be27ce4
Andre Bacelar <abacelar@outlook.com>
2022-09-26 21:17:23 -0300

    Added modulos and refactored store

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,6 +1,6 @@
 <template>
   <Box>
-    <div class="columns">
+    <div class="columns clicavel" @click="tarefaClicada">
       <div class="column is-4">
         {{ tarefa.descricao || "Tarefa sem descrição" }}
       </div>
@@ -20,6 +20,7 @@ import Box from "./Box.vue";
 
 export default defineComponent({
   name: "Tarefa",
+  emits: ["aoTarefaClicada"],
   components: {
     Cronometro,
     Box,
@@ -30,5 +31,16 @@ export default defineComponent({
       required: true,
     },
   },
+  methods: {
+    tarefaClicada(): void {
+      this.$emit("aoTarefaClicada", this.tarefa);
+    },
+  },
 });
 </script>
+
+<style scoped>
+.clicavel {
+  cursor: pointer;
+}
+</style>
